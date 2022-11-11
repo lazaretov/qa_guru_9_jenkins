d948b6e24cb27ddb184481c01260ce7fae27579b
Timofei <lazaretov03@gmail.com>
2022-11-11 15:11:55 +0300

    add selenoid remote

--- src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
+++ src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
@@ -15,7 +15,8 @@ public class RegistrationFormWithObjectsTests {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
