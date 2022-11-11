b2a54b466b203fcbf4512416fc2499308c6a6a32
Timofei <lazaretov03@gmail.com>
2022-11-11 15:00:52 +0300

    add allure steps

--- src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
+++ src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.lazaretov.testData.UserInfo.*;
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationFormWithObjectsTests {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
@@ -19,30 +20,32 @@ public class RegistrationFormWithObjectsTests {
 
     @Test
     void fillFormTest() {
-
-        registrationFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setPhone(phone)
-                .setDateOfBirth(day, month, year)
-                .setSubjects(subject)
-                .setHobbies(hobby)
-                .uploadPic(pic)
-                .setAddress(address, state, city)
-                .clickSubmit();
-
-        registrationFormPage.checkResultsTableVisible()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", email)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", phone)
-                .checkResult("Date of Birth", date)
-                .checkResult("Subjects", subject)
-                .checkResult("Hobbies", hobby)
-                .checkResult("Picture", pic)
-                .checkResult("Address", address)
-                .checkResult("State and City", state + " " + city);
+        step("Open and fill form", () -> {
+            registrationFormPage.openPage()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setPhone(phone)
+                    .setDateOfBirth(day, month, year)
+                    .setSubjects(subject)
+                    .setHobbies(hobby)
+                    .uploadPic(pic)
+                    .setAddress(address, state, city)
+                    .clickSubmit();
+        });
+        step("Check results", () -> {
+            registrationFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", phone)
+                    .checkResult("Date of Birth", date)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", hobby)
+                    .checkResult("Picture", pic)
+                    .checkResult("Address", address)
+                    .checkResult("State and City", state + " " + city);
+        });
     }
 }
