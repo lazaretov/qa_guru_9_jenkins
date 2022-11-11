cb19ea412cc83fd3f5f6a830a52fc57f027153d4
Timofei <lazaretov03@gmail.com>
2022-11-11 15:33:22 +0300

    addListener

--- src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
+++ src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
@@ -1,7 +1,9 @@
 package com.lazaretov.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.lazaretov.pages.RegistrationFormPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,7 @@ public class RegistrationFormWithObjectsTests {
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         //Configuration.holdBrowserOpen = true;
